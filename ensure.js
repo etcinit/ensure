7d7ed0eb798f08491095cad421c87249037f43e4
Eduardo Trujillo <ed@chromabits.com>
2014-08-13 17:17:31 -0400

    Add shield function
    - TODO: unit tests

--- ensure.js
+++ ensure.js
@@ -457,6 +457,52 @@ root = this;
 (function () {
     "use strict";
 
+    var shield;
+
+    /**
+     * Wrap around a function and perform type checks
+     *
+     * @param argumentSpec
+     * @param returnType
+     * @param innerFunction
+     * @param [thisContext]
+     * @returns {Function}
+     */
+    shield = function (argumentSpec, returnType, innerFunction, thisContext) {
+        ensure(argumentSpec, Array);
+        ensure(returnType, Function);
+        ensure(innerFunction, Function);
+
+        return function () {
+            var i,
+                returnValue;
+
+            // Check that we got the same number of arguments as specified in the spec
+            if (argumentSpec.length !== arguments.length) {
+                throw new Error('Function called with an invalid number of arguments');
+            }
+
+            // Check that every argument type matches
+            for (i in arguments) {
+                ensure(arguments[i], argumentSpec[i]);
+            }
+
+            // Call function
+            thisContext = thisContext || this;
+            returnValue = innerFunction.apply(thisContext, arguments);
+
+            // Check return type
+            ensure(returnValue, returnType);
+
+            return returnValue;
+        };
+    };
+
+    ensure.shield = shield;
+})();
+(function () {
+    "use strict";
+
     var TypeException;
 
     TypeException = function (expectedType, message) {
