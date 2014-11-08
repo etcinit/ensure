b2ab5fffd997bfc3aaa38b5286a7b513600fdf7f
Eduardo Trujillo <ed@chromabits.com>
2014-11-08 15:11:05 -0500

    Tests for shield

---
+++ test/ensure/shieldTest.js
@@ -0,0 +1,113 @@
+"use strict";
+
+var ensure = require('../../ensure'),
+
+    // A test function
+    myTestFunction = function (arg1, arg2) {
+        return arg1 && arg2;
+    };
+
+describe('ensure', function () {
+    // Make sure we set enforce back on after each test
+    afterEach(function (done) {
+        ensure.enforce = true;
+        done();
+    });
+
+    describe('#shield', function () {
+        it('should wrap around a function', function () {
+            var myFunction = function () {
+                return true;
+            },
+                myWrappedFunction;
+
+            myWrappedFunction = ensure.shield([], Boolean, myFunction);
+
+            myWrappedFunction.should.be.instanceOf(Function);
+        });
+
+        it('should type check its own arguments', function () {
+            (function () {
+                ensure.shield({}, null, null, null);
+            }).should.throw();
+        });
+
+        it('should type check arguments', function () {
+            var myWrappedFunction = ensure.shield([Boolean, Boolean], Boolean, myTestFunction);
+
+            (function (){
+                myWrappedFunction(true, false);
+            }).should.not.throw();
+
+            (function (){
+                myWrappedFunction(true, []);
+            }).should.throw();
+        });
+
+        it('should type check return values', function () {
+            var myWrappedFunction = ensure.shield([Boolean], Boolean, function (arg1) {
+                if (arg1) {
+                    return true;
+                }
+
+                return 8923;
+            });
+
+            (function (){
+                myWrappedFunction(true);
+            }).should.not.throw();
+
+            (function (){
+                myWrappedFunction(false);
+            }).should.throw();
+        });
+
+        it('should fail on argument number mismatch', function () {
+            var myWrappedFunction = ensure.shield([], Boolean, function () {
+                return true;
+            });
+
+            (function (){
+                myWrappedFunction();
+            }).should.not.throw();
+
+            (function (){
+                myWrappedFunction(false);
+            }).should.throw();
+        });
+
+        it('should not type check arguments when enforce if off', function () {
+            ensure.enforce = false;
+
+            var myWrappedFunction = ensure.shield([Boolean, Boolean], Boolean, myTestFunction);
+
+            (function (){
+                myWrappedFunction(true, false);
+            }).should.not.throw();
+
+            (function (){
+                myWrappedFunction(true, []);
+            }).should.not.throw();
+        });
+
+        it('should not type check return values if enforce is off', function () {
+            ensure.enforce = false;
+
+            var myWrappedFunction = ensure.shield([Boolean], Boolean, function (arg1) {
+                if (arg1) {
+                    return true;
+                }
+
+                return 8923;
+            });
+
+            (function (){
+                myWrappedFunction(true);
+            }).should.not.throw();
+
+            (function (){
+                myWrappedFunction(false);
+            }).should.not.throw();
+        });
+    });
+});
